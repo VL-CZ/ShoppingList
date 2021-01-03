d96c4ff410e8cd10df49485e34943ed5ea50b6c9
Vojtech Lengal <vojta.lengal@gmail.com>
2021-01-03 18:21:23 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h2>Shopping list</h2>
+    <div>
+        <table>
+            <tr>
+                <th>Item</th>
+                <th>Amount</th>
+                <th></th>
+                <th></th>
+            </tr>
+        </table>
+    </div>
+
+    <h3>Add item</h3>
+    <form method="post">
+        <input type="text" name="item"/>
+        <input type="number" name="amount"/>
+        <input type="submit" value="Submit"/>
+    </form>
+</body>
+</html>
